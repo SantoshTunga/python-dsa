3f23e4ab5feac6459b8085b7b3eb31ca68c982e7
SantoshTunga <santosh2tunga@gmail.com>
2026-05-30 15:30:03 +0530

    cycle detect algo

--- src/dsa/linkednode/linked_node.py
+++ src/dsa/linkednode/linked_node.py
@@ -1,5 +1,5 @@
 class LinkedNode:
-    def __init__(self, value: int, next_node: LinkedNode = None):
+    def __init__(self, value: int, next_node: LinkedNode | None = None):
         self.value = value
         self.next_node = next_node
 
@@ -7,13 +7,24 @@ class LinkedNode:
 def construct_linked_node() -> LinkedNode:
     l5 = LinkedNode(5)
     l4 = LinkedNode(4, l5)
+    l4 = LinkedNode(4, l5)
     l3 = LinkedNode(3, l4)
     l2 = LinkedNode(2, l3)
     l1 = LinkedNode(1, l2)
     return l1
 
 
-def print_linked_node(node: LinkedNode) -> None:
+def construct_cycle_linked_node() -> LinkedNode:
+    l5 = LinkedNode(5)
+    l4 = LinkedNode(4, l5)
+    l3 = LinkedNode(3, l4)
+    l5.next_node = l3
+    l2 = LinkedNode(2, l3)
+    l1 = LinkedNode(1, l2)
+    return l1
+
+
+def print_linked_node(node: LinkedNode | None):
     while node:
         print(f"node value = {node.value}")
         node = node.next_node
